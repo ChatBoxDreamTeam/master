ebd7e70accfcccc7517fb6545cb61d79462f5f9e
Math-Dzieu <dziedzic@et.esiea.fr>
2020-05-26 11:28:50 +0200

    Ajout de la JavaDoc

--- src/main/java/org/DreamTeam/Contact.java
+++ src/main/java/org/DreamTeam/Contact.java
@@ -3,40 +3,75 @@ package org.DreamTeam;
 import java.util.Comparator;
 
 public class Contact {
+
     private String firstName;
     private String lastName;
     private String phoneNumber;
 
+    /**
+     * Constructeur d'un Contact
+     * @param firstName prenom du contact
+     * @param lastName nom du contact
+     * @param phoneNumber numero du contact
+     */
     public Contact(String firstName, String lastName, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Methode permettant de récupérer le prenom du contact
+     * @return le prenom du contact
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Methode permettant de definir le prenom du contact
+     * @param firstName prenom du contact
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Methode permettant de récupérer le nom du contact
+     * @return le nom du contact
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Methode permettant de definir le nom du contact
+     * @param lastName nom du contact
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Methode permettant de récupérer le numero du contact
+     * @return le numero du contact
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Methode permettant de definir le numero du contact
+     * @param phoneNumber numero du contact
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Override de toString : Affichage du contact
+     * @return information du contact
+     */
     @Override
     public String toString() {
         return ("FirstName : "+this.getFirstName()+
@@ -44,6 +79,9 @@ public class Contact {
                 "\tPhone : "+ this.getPhoneNumber());
     }
 
+    /**
+     * Methode de comparaison des prenom pour avoir une liste de contact trier par prenom
+     */
     public static Comparator<Contact> contactFirstName = new Comparator<Contact>() {
 
         public int compare(Contact s1, Contact s2) {
@@ -54,6 +92,9 @@ public class Contact {
         }
     };
 
+    /**
+     * Methode de comparaison des prenom pour avoir une liste de contact trier par nom
+     */
     public static Comparator<Contact> contactLastName = new Comparator<Contact>() {
 
         public int compare(Contact s1, Contact s2) {
@@ -64,6 +105,9 @@ public class Contact {
         }
     };
 
+    /**
+     * Methode de comparaison des prenom pour avoir une liste de contact trier par telephone
+     */
     public static Comparator<Contact> contactPhoneNumber = new Comparator<Contact>() {
 
         public int compare(Contact s1, Contact s2) {
