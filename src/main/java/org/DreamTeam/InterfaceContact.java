f7aa36c0ccefa7d116680b6a8ec315fc261f4331
titouannwtt <titouan.wattelet@gmail.com>
2020-05-31 14:02:45 +0200

    JavaDoc + correction du clic droit lorsqu'aucune discussion existe

--- src/main/java/org/DreamTeam/InterfaceContact.java
+++ src/main/java/org/DreamTeam/InterfaceContact.java
@@ -7,10 +7,21 @@ import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 
-public class InterfaceContact extends Parent {
+/**
+ * <h1>InterfaceContact</h1>
+ * <h2>Attributs</h2>
+ * <ul>
+ *     <li>rectangle : Rectangle (Représente l'image d'une discussion)</li>
+ *     <li>discussionTitle : Text (Représente le nom de la discussion)</li>
+ *     <li>height : double (Dimension du rectangle pour l'image)</li>
+ *     <li>width : double (Dimension du rectangle pour l'image)</li>
+ * </ul>
+ */
 
+public class InterfaceContact extends Parent {
     Rectangle rectangle;
     Text discussionTitle;
+    int discussionId;
     double height, width;
 
     /**
@@ -30,31 +41,76 @@ public class InterfaceContact extends Parent {
         //System.out.println(discussionTitle);
         this.discussionTitle = new Text(this.rectangle.getWidth()+this.rectangle.getX(), this.rectangle.getY()+this.rectangle.getWidth()/2+5, discussionTitle);
         this.getChildren().addAll(rectangle, this.discussionTitle);
+        discussionId=nbDiscussion+1;
     }
 
-    public double getPositionX() {
-        return this.rectangle.getX();
-    }
-
-    public double getPositionY() {
-        return this.rectangle.getY();
-    }
-
+    //=========================================================
+    /**
+     * <h2>setPositionX</h2>
+     * @param coord coordonnée X de la discussion
+     */
     public void setPositionX(double coord) {
         this.rectangle.setX(coord);
         this.discussionTitle.setX(this.rectangle.getWidth()+this.rectangle.getX());
     }
+    /**
+     * <h2>getPositionX</h2>
+     * @return Renvoie la position X de la discussion
+     */
+    public double getPositionX() {
+        return this.rectangle.getX();
+    }
+    //=========================================================
 
+    /**
+     * <h2>setPositionY</h2>
+     * @param coord coordonnée Y de la discussion
+     */
     public void setPositionY(double coord) {
         this.rectangle.setY(coord);
         this.discussionTitle.setY(this.rectangle.getY()+this.rectangle.getWidth()/2+5);
     }
 
-    public Text getDiscussionTitle() {
-        return discussionTitle;
+    /**
+     * <h2>getPositionY</h2>
+     * @return Renvoie la position Y de la discussion
+     */
+    public double getPositionY() {
+        return this.rectangle.getY();
     }
 
+    //=========================================================
+    /**
+     * <h2>setDiscussionTitle</h2>
+     * @param discussionTitle titre de la discussion
+     */
     public void setDiscussionTitle(String discussionTitle) {
         this.discussionTitle.setText(discussionTitle);
     }
+
+    /**
+     * <h2>getDiscussionTitle</h2>
+     * @return Renvoie le titre de la discussion
+     */
+    public Text getDiscussionTitle() {
+        return discussionTitle;
+    }
+    //=========================================================
+
+    /**
+     * <h2>setDiscussionId</h2>
+     * @param discussionIdNew titre de la discussion
+     */
+    public void setDiscussionId(int discussionIdNew) {
+        this.discussionId=discussionIdNew;
+    }
+
+    /**
+     * <h2>getDiscussionId</h2>
+     * @return discussionId titre de la discussion
+     */
+    public int getDiscussionId() {
+        return this.discussionId;
+    }
+    //=========================================================
 }
--- src/main/java/org/DreamTeam/InterfaceDiscussion.java
+++ src/main/java/org/DreamTeam/InterfaceDiscussion.java
@@ -9,8 +9,19 @@ import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 
-public class InterfaceDiscussion extends Parent implements DiscussionListener {
+/**
+ * <h1>InterfaceDiscussion</h1>
+ * <h2>Attributs</h2>
+ * <ul>
+ *     <li>ListeDiscussion : ArrayList<Discussion> (Tableau des discussions)</li>
+ *     <li>interfaceContactArrayList : ArrayList<InterfaceContact> (Tableau des interfaces de discussion (=discussions graphiquement))</li>
+ *     <li>ic : InterfaceContact (Interace en cours d'utilisation par la classe lors des fonctions, etc.)</li>
+ *     <li>height : double (Hauteur de la partie gauche de la fenetre (interface discussion))</li>
+ *     <li>width : double (Largeur de la partie gauche de la fenetre (interface discussion))</li>
+ * </ul>
+ */
 
+public class InterfaceDiscussion extends Parent implements DiscussionListener {
     private ArrayList<Discussion> ListeDiscussion;
     private ArrayList<InterfaceContact> interfaceContactArrayList;
     private InterfaceContact ic;
@@ -52,6 +63,11 @@ public class InterfaceDiscussion extends Parent implements DiscussionListener {
         //deplace(width);
     }
 
+    /**
+     * <p>Permet d'ajouter une nouvelle discussion à la liste
+     * </p>
+     * @param o nouvel élément à ajouter
+     */
     @Override
     public void update(Object o) {
         this.ListeDiscussion.add((Discussion) o);
@@ -59,12 +75,22 @@ public class InterfaceDiscussion extends Parent implements DiscussionListener {
         i = i+1;
     }
 
+    /**
+     * <p>Créer graphiquement la discussion
+     * </p>
+     * @param titre titre de la discussion
+     * @param nbDiscussion nombre de discussion actuellement existant
+     */
     private void newInterfaceContact(String titre, int nbDiscussion) {
         ic = new InterfaceContact(this.width/5, this.width/5, titre, nbDiscussion);
         interfaceContactArrayList.add(ic);
         this.getChildren().add(ic);
     }
 
+    /**
+     * <p>Permet de replacer correctement graphiquement les discussions lors d'une suppression d'élément
+     * </p>
+     */
     public void refreshUI() {
         for(int i=0; i<interfaceContactArrayList.size(); i++) {
             ic=interfaceContactArrayList.get(i);
@@ -73,14 +99,29 @@ public class InterfaceDiscussion extends Parent implements DiscussionListener {
         }
     }
 
+    /**
+     * <p>Permet d'obtenir le nombre de discussion existante.
+     * </p>
+     * @return nombre de discussion existante
+     */
     public int getNumberOfDiscution() {
         return interfaceContactArrayList.size();
     }
 
+    /**
+     * <p>Permet d'obtenir la liste des discussions
+     * </p>
+     * @return liste des discussions
+     */
     public ArrayList<InterfaceContact> getInterfaceContactArrayList(){
         return interfaceContactArrayList;
     }
 
+    /**
+     * <p>Permet de supprimer une discussion
+     * </p>
+     * @param interfaceContact  discussions
+     */
     public void deleteDiscussion(InterfaceContact interfaceContact) {
         interfaceContactArrayList.remove(interfaceContact);
         this.getChildren().remove(interfaceContact);
@@ -88,8 +129,22 @@ public class InterfaceDiscussion extends Parent implements DiscussionListener {
         refreshUI();
     }
 
+    /**
+     * <p>Permet de créer une nouvelle discussion
+     * </p>
+     */
     public void createDiscussion() {
         newInterfaceContact("Discussion " + String.valueOf(i), i);
+        i = i +1;
+    }
+
+    /**
+     * <p>Permet de créer une nouvelle discussion
+     * </p>
+     * @param name Permet de donner un nom à une discussion
+     */
+    public void createDiscussion(String name) {
+        newInterfaceContact(""+name, i);
         i = i +1;
     }
 }
